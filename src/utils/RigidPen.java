dc6ce69d3be0a5456fb9f4f674b47d54f23c5b72
Celdir <michael.earl7769@gmail.com>
2018-01-27 13:43:41 -0600

    new useless files

---
+++ src/utils/RigidPen.java
@@ -0,0 +1,18 @@
+package utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class RigidPen extends PenType {
+
+	public void input(InputStream is) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void print(OutputStream os) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+}

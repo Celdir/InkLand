11c03b0c5b0e1e1ccb5bb5ec1c5d267486fc159b
EvModder <evmodder@gmail.com>
2018-01-27 16:24:13 -0600

    setting file

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,34 +1,29 @@
 package server;
 
 import java.io.IOException;
-import java.util.Scanner;
 import client.Orientation;
 import serverAPI.*;
 import utils.BodyList;
+import utils.Settings;
 import utils.Utils;
 
 public class ServerMain{
 	BodyList bodyList;
-
+	static Settings settings;
 	public static void main(String[] args){
-		Connection server = new ServerSide(new MessageReceiver(){
+		settings = new Settings();
+		new ServerSide(new MessageReceiver(){
 			@Override
 			public void receiveMessage(String message) {
 				Orientation orientation = new Orientation();
 				try{
 					orientation.input(Utils.toInputStream(message));
-//					bodyList.update(orientation);
+					//TODO: bodyList.update(orientation);
 				}
 				catch(IOException e){
 					e.printStackTrace();
 				}
 			}
-		});
-
-		Scanner scan = new Scanner(System.in);
-		while(scan.hasNextLine()){
-			server.println(scan.nextLine());
-		}
-		scan.close();
+		}, settings);
 	}
 }
--- src/utils/Settings.java
+++ src/utils/Settings.java
@@ -19,13 +19,26 @@ public class Settings {
 	public boolean getBoolean(String key){
 		return Boolean.parseBoolean(map.get(key.toLowerCase()));
 	}
+	public boolean getBoolean(String key, boolean def){
+		String val = map.get(key.toLowerCase());
+		return val == null ? def : Boolean.parseBoolean(val);
+	}
+
 	public int getInt(String key){
 		String val = map.get(key.toLowerCase());
 		return val != null && val.matches("\\d+") ? Integer.parseInt(val) : 0;
 	}
+	public int getInt(String key, int def){
+		String val = map.get(key.toLowerCase());
+		return val != null && val.matches("\\d+") ? Integer.parseInt(val) : def;
+	}
 	
 	public String getString(String key){
 		return map.get(key.toLowerCase());
+	}
+	public String getString(String key, String def){
+		String val = map.get(key.toLowerCase());
+		return val == null ? def : val;
 	}
 	
 	public void set(String key, Object value){

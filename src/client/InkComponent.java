5ad469701dd6c1561c482b150b11e603efbab0d7
jdl3569 <jdl3569@tamu.edu>
2018-01-27 16:23:56 -0600

    Commit #~8

--- src/client/InkComponent.java
+++ src/client/InkComponent.java
@@ -1,7 +1,12 @@
 package client;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.geom.Point2D;
+
 import javax.swing.JComponent;
+
+import utils.Blot;
 import utils.BodyList;
 
 public class InkComponent extends JComponent {
@@ -9,9 +14,32 @@ public class InkComponent extends JComponent {
 
 	BodyList list;
 	Orientation myPosition;
+	boolean rotLocked = true;
 
 	@Override protected void paintComponent(Graphics g) {
-		// TODO Auto-generated method stub
-		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		for(Blot b : list.blots) {
+			g.setColor(b.fill);
+			int npts = b.bound.points.size();
+			int[] xs = new int[npts];
+			int[] ys = new int[npts];
+			for(int i = 0; i < npts; ++i) {
+				Point2D.Double pt = myPosition.world2local(b.bound.points.get(i), rotLocked);
+				xs[i] = local2pixel(pt.x, false);
+				ys[i] = local2pixel(pt.y, true);
+			}
+			g.fillPolygon(xs, ys, npts);
+		}
+	}
+
+	private int local2pixel(double pos, boolean vert) {
+		if(vert) pos *= -1;
+		pos *= getScale();
+		return (int)pos + (vert ? getHeight() : getWidth())/2;
+	}
+
+	private double getScale() {
+		return 1;
 	}
 }

ff7af2f652322487e8ab549d945652763eb15d9c
EvModder <evmodder@gmail.com>
2018-01-27 16:09:54 -0600

    half done work

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,14 +1,27 @@
 package server;
 
+import java.io.IOException;
 import java.util.Scanner;
+import client.Orientation;
 import serverAPI.*;
+import utils.BodyList;
+import utils.Utils;
 
 public class ServerMain{
+	BodyList bodyList;
+
 	public static void main(String[] args){
 		Connection server = new ServerSide(new MessageReceiver(){
 			@Override
 			public void receiveMessage(String message) {
-				System.out.println("Received: "+message);
+				Orientation orientation = new Orientation();
+				try{
+					orientation.input(Utils.toInputStream(message));
+//					bodyList.update(orientation);
+				}
+				catch(IOException e){
+					e.printStackTrace();
+				}
 			}
 		});
 
--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,5 +1,10 @@
 package utils;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 public class Utils{
-	
+	public static InputStream toInputStream(String str){
+		return new ByteArrayInputStream(str.getBytes());
+	}
 }

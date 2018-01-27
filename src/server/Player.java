451f1aa0aeb18cd9016b2248f328cb785e099e1f
EvModder <evmodder@gmail.com>
2018-01-27 16:37:54 -0600

    player class for server

---
+++ src/server/Player.java
@@ -0,0 +1,10 @@
+package server;
+import client.Orientation;
+
+public class Player{
+	public final Orientation orientation;
+
+	Player(){
+		orientation = new Orientation();
+	}
+}
--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,29 +1,37 @@
 package server;
 
 import java.io.IOException;
-import client.Orientation;
+import java.util.HashMap;
 import serverAPI.*;
 import utils.BodyList;
 import utils.Settings;
 import utils.Utils;
 
-public class ServerMain{
+public class ServerMain implements MessageReceiver{
+	static void main(String[] args){ new ServerMain(); }
+
 	BodyList bodyList;
-	static Settings settings;
-	public static void main(String[] args){
+	Settings settings;
+	HashMap<Integer, Player> players;
+	
+	ServerMain(){
 		settings = new Settings();
-		new ServerSide(new MessageReceiver(){
-			@Override
-			public void receiveMessage(String message) {
-				Orientation orientation = new Orientation();
-				try{
-					orientation.input(Utils.toInputStream(message));
-					//TODO: bodyList.update(orientation);
-				}
-				catch(IOException e){
-					e.printStackTrace();
-				}
-			}
-		}, settings);
+		players = new HashMap<Integer, Player>();
+		new ServerSide(this, settings);
+		
+		
+	}
+
+	@Override
+	public void receiveMessage(String message) {
+		int i = message.indexOf(' '), id = Integer.parseInt(message.substring(0, i));
+		message = message.substring(i+1);
+		try{
+			// Assume all messages from a client are just their orientation
+			players.get(id).orientation.input(Utils.toInputStream(message));
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 }

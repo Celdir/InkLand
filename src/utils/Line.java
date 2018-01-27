9bcdc87f6510475ec5a69867f20722ead8bbb8d3
Celdir <michael.earl7769@gmail.com>
2018-01-27 15:55:14 -0600

    i/o for lines and blots

--- src/utils/Line.java
+++ src/utils/Line.java
@@ -4,16 +4,36 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Line implements Transferable {
 	public final ArrayList<Point2D.Double> points = new ArrayList<>();
+
+	boolean completed;
 	
-	public void print(OutputStream os) throws IOException {
-		// TODO Auto-generated method stub
-		
-	}
 	public void input(InputStream is) throws IOException {
-		// TODO Auto-generated method stub
-		
+		points.clear();
+		String coords = "";
+		char c;
+		while ((c = (char) is.read()) != '|') {
+			coords += c;
+		}
+
+		Scanner s = new Scanner(coords);
+		while (s.hasNextDouble()) {
+			double x = s.nextDouble();
+			double y = s.nextDouble();
+			points.add(new Point2D.Double(x, y));
+		}
 	}
+
+	public void print(OutputStream os) throws IOException {
+		String out = "";
+		for (Point2D.Double p : points) {
+			out += p.x + " " + p.y + " ";
+		}
+		out += "| ";
+		os.write(out.getBytes());
+	}
+
 }
